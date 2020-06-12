cd88bb370f747002b6aaa1dc02b7088f0dbaf1e3
orekyuu <orekyuu@gmail.com>
2020-06-12 20:35:45 +0900

    Change to web mvc servlet stack.

--- src/test/java/org/orekyuu/nozomi/presentation/controller/ProjectsControllerTest.java
+++ src/test/java/org/orekyuu/nozomi/presentation/controller/ProjectsControllerTest.java
@@ -1,38 +1,39 @@
 package org.orekyuu.nozomi.presentation.controller;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.orekyuu.nozomi.domain.project.Project;
 import org.orekyuu.nozomi.domain.project.ProjectId;
 import org.orekyuu.nozomi.domain.project.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
 
 import static org.mockito.Mockito.doReturn;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@ExtendWith(SpringExtension.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class ProjectsControllerTest {
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ProjectsControllerTest {
 
     @Autowired
-    WebTestClient client;
+    MockMvc client;
     @MockBean
     ProjectRepository projectRepository;
 
     @Test
-    void index() {
+    void index() throws Exception {
         var data = List.of(new Project(new ProjectId("id"), "test name"));
         doReturn(data).when(projectRepository).findAll();
 
-        client.get().uri("/api/projects").exchange()
-                .expectStatus().isOk()
-                .expectBody()
-                .jsonPath("[0].id").isEqualTo("id")
-                .jsonPath("[0].name").isEqualTo("test name");
+        client.perform(get("/api/projects"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("[0].id").value("id"))
+                .andExpect(jsonPath("[0].name").value("test name"));
     }
 }
